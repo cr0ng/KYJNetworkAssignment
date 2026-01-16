e9a4863ab5c9b8fd331b9563e98ffe55d9ec93c0
yujin <yj98k@naver.com>
2026-01-16 17:59:59 +0900

    캐릭터 시작하기

---
+++ Source/KYJ_Network_Project/Public/Character/PlayerCharacter.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class KYJ_NETWORK_PROJECT_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(Server, Reliable)
+	void Server_AddScore(int32 Point);
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Score")
+	void AddPointScore();
+
+};
